552f6fbfcad4eece36d8b12774b65b912fe4ead9
Khoa Hoang <khoa.hoanghoa@nashtechglobal.com>
2025-10-25 08:57:43 +0700

    Demo 1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with hhkhoa96
+          Learn React with hhkhoa96 version 1
         </a>
       </header>
     </div>
