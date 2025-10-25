f86b55612b5e2f5b834086ed551cebfaafa6e828
Khoa Hoang <khoa.hoanghoa@nashtechglobal.com>
2025-10-25 10:20:11 +0700

    Try main

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with hhkhoa96 version 2
+          Learn React with hhkhoa96 version 3
         </a>
       </header>
     </div>
